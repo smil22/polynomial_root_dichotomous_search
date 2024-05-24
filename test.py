c6294d0d8ea997ac4e9e009dc994873bdbf235ae
Salim <92552456+smil22@users.noreply.github.com>
2024-05-24 14:30:51 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,8 @@
+import library
+
+p = [4,0,-6,2] #p(x) = 4x³-6x+2
+a,b,tol = -2,0,1e-5
+
+root = library.racine_dichotomie(p, a, b, tol)
+
+print('Root: {0:3.4f}\tEvaluation: {1:3.4e}'.format(root,library.horner(p,root)))
